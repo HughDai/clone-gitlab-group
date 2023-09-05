6d2845c542dac245306d79db34d0a4a045f69f9d
hugh <cynicism2011@gmail.com>
2023-09-05 16:47:40 +0800

    Update clone_gitlab_group.sh

--- clone_gitlab_group.sh
+++ clone_gitlab_group.sh
@@ -8,7 +8,7 @@
 
 # ====================================================================
 # manual
-# Set a environment variable GITLAB_SESSION like "21ab2d2c3cbc4dfc33cafb14084450bf" which stored in the browsers cookies as a value to key "_gitlab_session"
+# Set an environment variable GITLAB_SESSION like "21ab2d2c3cbc4dfc33cafb14084450bf" which stored in the browsers cookies as a value to key "_gitlab_session"
 # chmod 777 clone_gitlab_group.sh
 # vim ~/.bashrc
 # alias clone ~/clone_gitlab_group.sh
